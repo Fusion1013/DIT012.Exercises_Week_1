f6280cb9ae1893e0188040fe33a8484d3e5e2727
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2019-11-06 19:29:56 +0100

    Update Ex4Arrays.java

--- src/exercises/Ex4Arrays.java
+++ src/exercises/Ex4Arrays.java
@@ -22,6 +22,47 @@ public class Ex4Arrays {
 
     void program() {
         // Write code here
+        out.print("Length: ");
 
+        int x = sc.nextInt();
+        int[] a = new int[x];
+
+        out.print("Numbers: ");
+
+        for (int i = 0; i < x; i++)
+        {
+            int n = sc.nextInt();
+            a[i] = n;
+        }
+
+        out.print("Numbers: ");
+        PrintArray(a);
+
+        out.print("Input value to find: ");
+        int n = sc.nextInt();
+        Boolean found = false;
+        for (int i = 0; i < a.length; i++)
+        {
+            if (a[i] == n)
+            {
+                out.print("Value " + n + " is at index " + i);
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            out.print("Value not found");
+        }
+    }
+
+    void PrintArray(int[] a)
+    {
+        String ar = "";
+        for (int i : a)
+        {
+            ar += i + ",";
+        }
+        ar = ar.substring(0, ar.length() - 1);
+        out.println("Array is: [" + ar + "]");
     }
 }
